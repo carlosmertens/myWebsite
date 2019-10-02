464aed519062201e247dd4b225fd65b8ea047e8e
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-02 21:22:00 +0200

    Migrate new model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    # Create an image field
+    image = models.ImageField(upload_to='images/')
+    # Create the description field
+    description = models.CharField(max_length=250)
