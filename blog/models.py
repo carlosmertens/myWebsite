b6bbf6f287aabe6d5bf37ecb63873708099aeb39
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-04 11:06:14 +0200

    Migrate Blog model

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,5 @@ class Blog(models.Model):
 
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
-    summary = models.CharField(max_length=200)
+    summary = models.TextField(max_length=200)
     image = models.ImageField(upload_to='images/')
