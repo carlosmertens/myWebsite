faea24f136a4a539b35de2bcfa43ec2579180231
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-08 01:12:54 +0200

    Connect database portfolio to the website

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Portfolio
 
 
 def index(request):
-    return render(request, 'portfolio/index.html')
+    projects = Portfolio.objects
+    return render(request, 'portfolio/index.html', {'projects': projects})
