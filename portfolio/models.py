3fa618bcdaa7d2a7cdde4f792e19a231b11ac579
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-07 23:43:40 +0200

    Upgrade database fields

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,4 +5,5 @@ class Portfolio(models.Model):
     """Create a portfolio entry."""
 
     image = models.ImageField(upload_to='images/')
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=350)
+    gitHub = models.URLField(default='https://github.com/carlosmertens')
