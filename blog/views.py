db06d5c2ca799407396d68fd197e5e0c5d3f2b94
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-08 23:35:39 +0200

    Connect database to the blog page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 
 def allBlogs(request):
-    return render(request, 'blog/index.html')
+    posts = Blog.objects
+    return render(request, 'blog/index.html', {"posts": posts})
