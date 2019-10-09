463f359a4b41a02cd9d768c2bedf0bc37e86f0f1
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-09 13:53:29 +0200

    Set up a stand alone blog page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.allBlogs, name='allBlogs'),
+    path('<int:blog_id>/', views.theBlog, name='theBlog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
 def allBlogs(request):
     posts = Blog.objects
     return render(request, 'blog/index.html', {"posts": posts})
+
+
+def theBlog(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/theBlog.html')
