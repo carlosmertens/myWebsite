9d6e0dc5faa135835907d40eb5e38c7fb87d6a19
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-10 12:05:35 +0200

    Create stand alone blog page

--- myWebsite/settings.py
+++ myWebsite/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o$5lxl%_!!5q^j!8@tnuy-a9kk1q#0dgy@bp4mksd!gbstk5s4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
