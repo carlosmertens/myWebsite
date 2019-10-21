afc06b175ceccff48bb1ecb418b3270e44d3dec7
Carlos Mertens <mertenscarlos@yahoo.com>
2019-10-21 21:08:10 +0200

    Create header/footer base template

--- myWebsite/settings.py
+++ myWebsite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'myWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['myWebsite/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
